40f53430c270ab26a8ea7ea3f53e675e254b5f6f
Oscar Moreno <oscar.moreno@cti.espol.edu.ec>
2020-03-12 11:28:26 -0500

    update datasets.py

--- datasets.py
+++ datasets.py
@@ -8,6 +8,7 @@ class BDataset:
 
   def __init__(self):
     self.pdfsfolder = 'pdfs/'
+    self.basedir = 'datasets/'
     self.csv = csvClient.ADataset()
     self.currentsession = ''
     self.indiceName = 'sesiones.csv'
@@ -22,8 +23,11 @@ class BDataset:
         self.currentAge = folder[-4:]
         path = os.path.join(folderName, folder)
         for foldern2, subf2, fnames2 in os.walk(path):
-          self.currentMonth = foldern2[-4:]
           for folder2 in subf2:
+            if self.currentAge == '2013' or self.currentAge == '2014':
+              self.currentMonth = folder2
+            else:
+              self.currentMonth = folder2[:-4]
             path2 = os.path.join(foldern2, folder2)
             for foldern3, subf3, fnames3 in os.walk(path2):
               for folder3 in subf3:
@@ -38,25 +42,35 @@ class BDataset:
                     logging.info('Nombre: ' + files)
                     logging.info('Pdf: ' + path4)
                     logging.info('')
-                    logging.info('_______________')
-                    logging.info('')
                     print(path4)
                     self.csv.pdfdir = path4
                     try:
-                      dfs = self.csv.get_dfs()
-                      dfconcat = self.csv.concat_dfs(dfs)
                       info = self.csv.get_info(count, self.currentAge, self.currentMonth)
                       name = self.csv.sesionName
-                      self.csv.get_csv(dfconcat, name)
-                      self.sesDf = self.sesDf.append(pd.Series(info, index=self.colNames), ignore_index=True)
+                      verPath = self.basedir + name + '.csv'
+                      if os.path.exists(verPath):
+                        logging.info('Ya existe: ' + name)
+                        self.sesDf = self.sesDf.append(pd.Series(info, index=self.colNames), ignore_index=True)
+                        self.get_sesiones_csv() 
+                      else:
+                        dfs = self.csv.get_dfs()
+                        dfconcat = self.csv.concat_dfs(dfs)
+                        self.csv.get_csv(dfconcat, name)
+                        self.sesDf = self.sesDf.append(pd.Series(info, index=self.colNames), ignore_index=True)
+                        self.get_sesiones_csv()
+                      logging.info('_______________')
+                      logging.info('')
                     except Exception as e:
                       print(e)
                       print(self.sesDf)
-                      path = self.csv.basedir + self.indiceName
-                      self.sesDf.to_csv(path, index=False)
+                      self.get_sesiones_csv()
                       logging.warning('Error con el pdf: ' + files)
                       raise e
                     count += 1
+
+  def get_sesiones_csv(self):
+    path = self.indiceName
+    self.sesDf.to_csv(path, index=False)
 
 if __name__ == '__main__':
     logging.basicConfig(filename='datasets_log.log', filemode='w', level=logging.INFO, format='%(asctime)s %(message)s')
